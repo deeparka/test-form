f0c7f9d0c46c4ec00725b64ebe495701e77a277b
deeparka <arkadeep180@gmail.com>
2022-03-02 19:15:49 +0530

    added some styling

--- src/components/DetailsForm.js
+++ src/components/DetailsForm.js
@@ -1,35 +1,25 @@
 const DetailsForm = () => {
     return (
         <form action="" className="form">
-            <input
-                type="text"
-                value="first-name"
-                placeholder="First Name"
-                required
-            />
-            <input
-                type="text"
-                value="last-name"
-                placeholder="Last Name"
-                required
-            />
-            <input
-                type="email"
-                value="email"
-                placeholder="Email Address"
-                required
-            />
-            <input
-                type="password"
-                value="password"
-                placeholder="Password"
-                required
-            />
-            <input
-                type="submit"
-                className="claim"
-                value="Claim your free trial"
-            />
+            <div>
+                <input type="text" placeholder="First Name" required />
+            </div>
+            <div>
+                <input type="text" placeholder="Last Name" required />
+            </div>
+            <div>
+                <input type="email" placeholder="Email Address" required />
+            </div>
+            <div>
+                <input type="text" placeholder="Password" required />
+            </div>
+            <div>
+                <input
+                    type="submit"
+                    className="claim"
+                    value="Claim your free trial"
+                />
+            </div>
             <div className="accept">
                 By clicking the button, you are agreeing to our
                 <span>Terms and Services</span>
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,18 +4,22 @@ const Home = () => {
     return (
         <div className="home">
             <div className="description">
-                <div className="headline">Learn to code by watching others</div>
+                <div className="headline">
+                    Learn to code by <br /> watching others
+                </div>
                 <p>
                     See how experienced developers solve problems in real-time.
                     Watching scripted tutorials is great, but understanding how
                     developers think is invaluable.
                 </p>
             </div>
-            <div className="offer">
-                Try it free 7 days then $20/mo. thereafter
-            </div>
-            <div className="details-form">
-                <DetailsForm />
+            <div className="offer-details-form">
+                <div className="offer">
+                    <span>Try it free 7 days</span> then $20/mo. thereafter
+                </div>
+                <div className="details-form">
+                    <DetailsForm />
+                </div>
             </div>
         </div>
     );
